92fd0cd6b67b4aa3a232d534164bafbcc489b50d
M00NPANG <moonsky1124@naver.com>
2023-03-09 15:33:53 +0900

    Update test_server.c

--- networkPrograming/chap02/test_server.c
+++ networkPrograming/chap02/test_server.c
@@ -25,7 +25,31 @@ int main(void)
 		printf("<ERROR> socket  오류 발생(code=%d)\n", errorCode);
 	}
 
-	bind();		// 주소 설정...
+	// 서버의 주소와 포트를 할당
+	SOCKADDR_IN severAddr;		//IPv4 주소 체계
+	memset(&severAddr, 0, sizeof(severAddr));		// 0으로 채워주기 위한 코드
+	severAddr.sin_family = AF_INET;		// IPv4 주소 체계
+	severAddr.sin_port = htons(9000);	// big endian
+	severAddr.sin_addr.S_un.S_addr = inet_addr("1.2.3.4");
+
+	/*
+	#define s_addr  S_un.S_addr
+	#define s_host  S_un.S_un_b.s_b2    // host on imp
+	#define s_net   S_un.S_un_b.s_b1    // network
+	#define s_imp   S_un.S_un_w.s_w2    // imp
+	#define s_impno S_un.S_un_b.s_b4    // imp #
+	#define s_lh    S_un.S_un_b.s_b3    // logical host
+	*/
+
+	bind(serverSocket, (SOCKADDR*)&severAddr, sizeof(severAddr));
+	// severAddr가 bind에서는 제네럴 타입이어야 하기 떄문에 SOCKADDR*로 캐스팅해주어야 함.  (타입 맞추기)
+	// SOCKADDR_IN port와 addr 구조체가 들어있기 떄문에 얘로 선언하고 후 바꿔주는것이 편함
+
+	unsigned long host_addr = 0x12345678;			//32bits
+	unsigned long net_addr = htonl(host_addr);		// network byte ordeering = BIG ENDIAN (상위 바이트를 하이 번지에 저장)
+	// htonl() -> 본인이 어떤 엔디안인지 확인
+	printf("> host byte ordering = 0x%x : \n", host_addr);
+	printf("> network byte ordering = 0x%x (Big Endian) : \n", net_addr)
 
 	listen();		// 서버 준비 완료
 
